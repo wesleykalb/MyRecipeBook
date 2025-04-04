0406383b88fa56655a15e0aecf31df638212ec6d
wesleykalb <wesleykalb10@gmail.com>
2025-04-04 15:18:31 -0300

    feat: adding

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Errors { get; set; }
+
+        public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
+        public ResponseErrorJson(string error) 
+        {
+            Errors = [error];
+        }
+    }
+}
