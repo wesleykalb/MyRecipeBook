9bf3cb1f3caf370134d5fdaf01ae369998ff2b04
wesleykalb <wesleykalb10@gmail.com>
2025-04-04 16:00:19 -0300

    feat: adding files estructure

---
+++ src/Backend/MyRecipeBook.Infraestructure/DataAccess/Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyRecipeBook.Infraestructure.DataAccess.Repositories
+{
+    public interface IUnitOfWork
+    {
+        public Task Commit();
+    }
+}
---
+++ src/Backend/MyRecipeBook.Infraestructure/DependencyInjectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infraestructure.DataAccess;
+using MyRecipeBook.Infraestructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infraestructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext_MySqlServer(services, configuration);
+            AddRepositories(services);
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        }
+
+        private static void AddDbContext_MySqlServer(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Connection");
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 41));
+           
+            services.AddDbContext<MyRecipeBookDbContext>(options =>
+            {
+                options.UseMySql(connectionString, serverVersion);
+            });
+        }
+    }
+}
