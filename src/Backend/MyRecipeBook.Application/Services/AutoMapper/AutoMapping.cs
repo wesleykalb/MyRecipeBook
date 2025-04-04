8180ec2f31a3afe29ef926be45ce4d0c6fe4bc4b
wesleykalb <wesleykalb10@gmail.com>
2025-04-04 15:58:37 -0300

    feat: adding files application

---
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.Services.AutoMapper
+{
+    class AutoMapping : Profile
+    {
+        public AutoMapping() 
+        {
+            RequestToDomain();
+
+        }
+
+        private void RequestToDomain() 
+        {
+            CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+                .ForMember(src => src.Password, opt => opt.Ignore());
+        }
+    }
+}
