9e9bbfb1fb732f246818b533bab27dba20ed9a4b
wesleykalb <wesleykalb10@gmail.com>
2025-04-09 15:10:06 -0300

    feat: adding api files test

--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -2,6 +2,8 @@ using MyRecipeBook.API.Filters;
 using MyRecipeBook.API.Middleware;
 using MyRecipeBook.Application;
 using MyRecipeBook.Infraestructure;
+using MyRecipeBook.Infraestructure.Extensions;
+using MyRecipeBook.Infraestructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,4 +39,24 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+MigrateDatabase();
+
 app.Run();
+
+void MigrateDatabase()
+{
+    if (builder.Configuration.IsUnitTestEnvironment())
+        return;
+
+    var connectionString = builder.Configuration.ConnectionString();
+
+    var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>()
+        .CreateScope();
+
+    DatabaseMigration.Migrate(connectionString, serviceScope.ServiceProvider);
+}
+
+public partial class Program
+{
+
+}
