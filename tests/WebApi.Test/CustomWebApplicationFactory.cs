8a9024e8f8277b8af6fea0b9dba4c3918c0486d2
wesleykalb <wesleykalb10@gmail.com>
2025-04-09 15:21:11 -0300

    feat: adding files for test on api

---
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Infraestructure.DataAccess;
+
+namespace WebApi.Test
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test")
+                .ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType == typeof(DbContextOptions<MyRecipeBookDbContext>));
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+                    var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+                    
+                    services.AddDbContext<MyRecipeBookDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryDbForTesting");
+                        options.UseInternalServiceProvider(provider);
+                    });
+                });
+        }
+    }
+}
---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using CommomTesteUtilities.Requests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using WebApi.Test.InlineData;
+using Xunit;
+
+namespace WebApi.Test.User.Register
+{
+    public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegisterUserTest(CustomWebApplicationFactory factory) => _httpClient = factory.CreateClient();
+        
+
+        [Fact]
+        public async Task Success()
+        { 
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var response = await _httpClient.PostAsJsonAsync("User", request);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+            await using var reponseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(reponseBody);
+
+            var responseDataName = responseData.RootElement.GetProperty("name").GetString();
+
+            responseData.RootElement.GetProperty("name").GetString().ShouldSatisfyAllConditions(
+                () => responseDataName.ShouldNotBeNull(),
+                () => responseDataName.ShouldBe(request.Name)
+            );
+        }
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Empty_Name(string culture)
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+                _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+            _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+
+            var response = await _httpClient.PostAsJsonAsync("User", request);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            await using var reponseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(reponseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+            errors.ShouldSatisfyAllConditions(
+                () => errors.Count().ShouldBe(1),
+                () => errors.First().GetString().ShouldBe(expectedMessage)
+            );
+        }
+
+    }
+}
